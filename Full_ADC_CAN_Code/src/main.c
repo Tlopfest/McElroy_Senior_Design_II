6a948f92f7d3683a86f5d9fbd873a47e376c28bc
Tlopfest <tlopfest@gmail.com>
2022-02-17 18:14:45 -0600

    Added full averaging code to main, fixed non-sending bug

--- Full_ADC_CAN_Code/src/main.c
+++ Full_ADC_CAN_Code/src/main.c
@@ -17,11 +17,13 @@ int led = 0;
 #define RTD_PORT 50 //Change this when selecting ports for RTD.
 #define RTD_PORT2 51 //Same here
 
-uint32_t input_1 = 0;
-uint32_t input_2 = 0;
+uint32_t input_1[100];
+uint32_t input_2[100];
+uint32_t input_1_avg = 0;
+uint32_t input_2_avg = 0;
 uint32_t avg = 0;
 uint32_t final_input = 0;
-char hex[8];
+int counter = 0;
 
 void PORT_init (void) {
 	PCC->PCCn[PCC_PORTD_INDEX ]|=PCC_PCCn_CGC_MASK; /* Enable clock for PORTD */
@@ -71,16 +73,16 @@ int main(void){
 	for(;;) {
 		convertAdcChan(12); /* Convert Channel AD12 to pot on EVB */ //WILL NEED TO SET CORRECT PORT LATER
 		while(adc_complete()==0){} /* Wait for conversion complete flag */
-		input_1 = read_adc_chx();
-		if (input_1 > 3750) { /* If result > 3.75V */
+		input_1[counter] = read_adc_chx();
+		if (input_1[counter] > 3750) { /* If result > 3.75V */
 			PTD->PSOR |= 1<<PTD0 | 1<<PTD16; /* turn off blue, green LEDs */
 			PTD->PCOR |= 1<<PTD15; /* turn on red LED */
 		}
-		else if (input_1 > 2500) { /* If result > 3.75V */
+		else if (input_1[counter] > 2500) { /* If result > 3.75V */
 			PTD->PSOR |= 1<<PTD0 | 1<<PTD15; /* turn off blue, red LEDs */
 			PTD->PCOR |= 1<<PTD16; /* turn on green LED */
 		}
-		else if (input_1 >1250) { /* If result > 3.75V */
+		else if (input_1[counter] >1250) { /* If result > 3.75V */
 			PTD->PSOR |= 1<<PTD15 | 1<<PTD16; /* turn off red, green LEDs */
 			PTD->PCOR |= 1<<PTD0; /* turn on blue LED */
 		}
@@ -90,15 +92,29 @@ int main(void){
 		//convertAdcChan(RTD_PORT2);
 		//while(adc_complete()==0){}
 		//input_2 = read_adc_chx();/* Get channel's conversion results in mv */
-		input_2=input_1; //TEMP FOR POTENTIIOMETER TESTING
-		avg = (input_1+input_2)/2;
-		//final_input = temp_finder(avg/1000);
-		final_input=avg; //TEMP UNTIL temp_finder IS SETUP
-		//convertAdcChan(29); /* Convert chan 29, Vrefsh */
-		//while(adc_complete()==0){} /* Wait for conversion complete flag */
-		//adcResultInMv_Vrefsh = read_adc_chx(); /* Get channel's conversion results in mv */
-		//FLEXCAN0_transmit_msg_test();
-		FLEXCAN0_transmit_msg_RTDs(input_1, input_2);
-		FLEXCAN0_transmit_msg_AVG(final_input, 0);
+		input_2[counter]=input_1[counter]; //TEMP FOR POTENTIIOMETER TESTING
+		if(counter<100){
+			counter++;
+		}
+		else{
+			for(int temp=0; temp<100; temp++){
+				input_1_avg = input_1_avg + input_1[temp];
+			}
+			input_1_avg = input_1_avg/100;
+			for(int temp=0; temp<100; temp++){
+				input_2_avg = input_2_avg + input_2[temp];
+			}
+			input_2_avg = input_2_avg/100;
+			counter=0;
+			avg = (input_1_avg+input_2_avg)/2;
+			//final_input = temp_finder(avg/1000);
+			final_input=avg; //TEMP UNTIL temp_finder IS SETUP
+			//convertAdcChan(29); /* Convert chan 29, Vrefsh */
+			//while(adc_complete()==0){} /* Wait for conversion complete flag */
+			//adcResultInMv_Vrefsh = read_adc_chx(); /* Get channel's conversion results in mv */
+			//FLEXCAN0_transmit_msg_test();
+			FLEXCAN0_transmit_msg_RTDs(input_1_avg, input_2_avg);
+			FLEXCAN0_transmit_msg_AVG(final_input, 0);
 		}
 	}
+}
